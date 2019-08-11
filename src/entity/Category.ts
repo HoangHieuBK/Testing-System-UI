aabc909345cee1dac6546106f2219ffa6ded2b30
bhhieu <bhhieu@cmc.com.vn>
2019-08-11 12:34:38 +0700

    first commit

---
+++ src/entity/Category.ts
@@ -0,0 +1,10 @@
+import { User } from './User';
+
+
+export class Category {
+  categoryId: number;
+  categoryName: string;
+  userCategory: User;
+  dateCreated: Date;
+  status: number;
+}
---
+++ src/entity/User.ts
@@ -0,0 +1,8 @@
+export class User {
+  userId: string;
+	fullName: string;
+	email: string;
+	mobile: string;
+	password: string;
+  status: number;
+}
